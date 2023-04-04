86cbb967848df0cf033eebd88073f30c9a72746c
Dennis Plosceanu <dennisplosceanu@gmail.com>
2023-04-04 14:01:42 +0300

    fixed undo dir

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -3,14 +3,14 @@ local utils = {}
 local Path = require('plenary.path')
 
 function utils.persistent_undo()
+    home = Path:new(Path.path.home)
     if pcall(function()
         vim.fn.system {
             'mkdir',
             '-p',
-            '~/.vim/temp_dirs/undodir'
+            home .. '/.vim/temp_dirs/undodir'
         }
     end) then
-        home = Path:new(Path.path.home)
         vim.o.undodir = home .. '/.vim/temp_dirs/undodir'
         vim.o.undofile = true
     else
